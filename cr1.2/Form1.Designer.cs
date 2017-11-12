135b3f3c574251c07b454bd02c58e0a0341e24e2
Sergey <letsdrumm@gmail.com>
2017-11-12 23:31:46 +0300

    G
    new

---
+++ cr1.2/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace cr1._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(11, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(191, 306);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 573);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Анализ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.listView2);
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Location = new System.Drawing.Point(1, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(409, 607);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Анализ";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(369, 19);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(40, 21);
+            this.comboBox2.TabIndex = 12;
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView2.Location = new System.Drawing.Point(207, 364);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(191, 206);
+            this.listView2.TabIndex = 5;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Символ";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Частота";
+            this.columnHeader4.Width = 66;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(11, 364);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(191, 206);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Символ";
+            this.columnHeader1.Width = 57;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Частота";
+            this.columnHeader2.Width = 79;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "0"});
+            this.comboBox1.Location = new System.Drawing.Point(155, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(47, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(208, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Дешифрованный второй текст";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(208, 353);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Частота №2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(207, 38);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 306);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Зашифрованный текст";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 353);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Частота №1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(299, 573);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 25);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Дешифровать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(207, 573);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 25);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Зашифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 616);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ cr1.2/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace cr1._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Dictionary<char, double> dict1 = new Dictionary<char, double>();
+        Dictionary<char, double> dict2 = new Dictionary<char, double>();        
+        public string alfavit = "1234567890";
+        static StreamReader sr = new StreamReader("text.txt");
+        static string text = sr.ReadToEnd();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FreqOne(text, alfavit);
+
+        }
+        public void FreqOne(string text, string alfavit)
+        {
+            char lowCh;
+            foreach (char ch in text)
+            {
+                lowCh = char.ToLower(ch);
+                for (int i = 0; i < alfavit.Length; i++)
+                {
+                    if (dict1.ContainsKey(lowCh))
+                        dict1[lowCh]++;
+                    else
+                        if (ch == alfavit[i])
+                            dict1.Add(lowCh, 1);
+                }
+            }
+
+            foreach (var item in dict1)
+                for (int i = 0; i < alfavit.Length; i++)
+                    if (item.Key == alfavit[i])
+                        listView1.Items.Add(new ListViewItem(new string[] { item.Key.ToString(), (item.Value / text.Length).ToString() }));
+        }
+
+        public void FreqTwo(string text, string alfavit)
+        {
+            int count = richTextBox1.Text.Length;
+            listView2.Items.Clear();
+            char lowCh;
+            foreach (char ch in text)
+            {
+                lowCh = char.ToLower(ch);
+                for (int i = 0; i < alfavit.Length; i++)
+                {
+                    if (dict2.ContainsKey(lowCh))
+                        dict2[lowCh]++;
+                    else
+                        if (ch == alfavit[i])
+                        dict2.Add(lowCh, 1);
+                }
+            }
+
+            foreach (var item in dict2)
+                for (int i = 0; i < alfavit.Length; i++)
+                    if (item.Key == alfavit[i])
+                        listView2.Items.Add(new ListViewItem(new string[] { item.Key.ToString(), (item.Value / count).ToString() }));
+        }        
+
+        public void CryptoAnalysis(Dictionary<char, double> dict1, Dictionary<char, double> dict2)
+        {
+            double value1 = dict1.Values.Max();
+            double value2 = dict2.Values.Max();
+            char a, b;
+            int index1 = 0;
+            int index2 = 0;
+            foreach (var item1 in dict1)
+                if (item1.Value == value1)
+                {
+                    a = item1.Key;
+                    index1 = alfavit.IndexOf(a);
+                }
+
+
+            foreach (var item2 in dict2)
+                if (item2.Value == value2)
+                {
+                    b = item2.Key;
+                    index2 = alfavit.IndexOf(b);
+                }
+
+            comboBox2.Text = ((index2 - index1 + alfavit.Length) % alfavit.Length).ToString();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            int key = Convert.ToInt32(comboBox1.Text);
+            StreamReader sr = new StreamReader("text2.txt");
+            string nums = sr.ReadToEnd();
+            string enc = "";
+            for (int i = 0; i < nums.Length; i++)
+            {
+                bool changed = false;
+                for (int j = 0; j < alfavit.Length; j++)
+                {
+                    if (nums[i] == alfavit[j])
+                    {
+                        int temp = (j + key) % alfavit.Length;
+                        enc = enc + alfavit[temp];
+                        changed = true;
+                        break;
+                    }
+                }
+                if (!changed)
+                    enc = enc + nums[i];
+            }            
+            richTextBox1.Text = enc;
+            FreqTwo(richTextBox1.Text, alfavit);
+            CryptoAnalysis(dict1, dict2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int key = Convert.ToInt32(comboBox2.Text);
+            string nums = richTextBox1.Text;
+            string enc = "";
+             for (int i = 0; i < nums.Length; i++)
+            {
+                bool changed = false;
+                for (int j = 0; j < alfavit.Length; j++)
+                {
+                    if (nums[i] == alfavit[j])
+                    {
+                        int temp = (j - key + alfavit.Length) % alfavit.Length;
+                        enc = enc + alfavit[temp];
+                        changed = true;
+                        break;
+                    }
+                }
+                if (!changed)
+                    enc = enc + nums[i];
+            }                        
+            textBox1.Text = enc;                       
+        }
+    }
+}
